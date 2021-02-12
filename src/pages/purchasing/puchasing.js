6afe2163e829b0172f050fea93e52973aa5a552b
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-02-12 17:24:28 -0300

    created container values

--- src/pages/purchasing/puchasing.js
+++ src/pages/purchasing/puchasing.js
@@ -23,6 +23,29 @@ const Purchasing = () => (
           pathbook={book2}
         />
       </div>
+      <div className='container-values'>
+        <ul className='container-values__items'>
+          <li className='container-values__item'>
+            <p>SubTotal</p>
+            <p>$25.00</p>
+          </li>
+          <li className='container-values__item'>
+            <p>Tax (GST 10%)</p>
+            <p>$2.5</p>
+          </li>
+          <li className='container-values__item'>
+            <p>Shipping</p>
+            <p>$0.00</p>
+          </li>
+          <li className='container-values__item line'>
+            <div></div>
+          </li>
+          <li className='container-values__item total'>
+            <p>Total amount</p>
+            <p>$27.50</p>
+          </li>
+        </ul>
+      </div>
     </div>
 );
 
